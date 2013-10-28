cd1fbb43160fe4cc4e9674139c3d1848fdfe139a
Jack Henahan <jhenahan@me.com>
2013-10-28 11:14:22 -0400

    All snippets view

---
+++ all.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+
+include "common/top.php";
+
+$getSamplesSql =
+    "SELECT
+     `u`.`pkUsername` AS 'user',
+     `sc`.`fldTitle` AS 'title',
+     `l`.`fldLanguageName` AS 'lang',
+     `sc`.`fldDescription` AS 'description',
+     `sc`.`fldSampleCode` AS 'code'
+     FROM
+     `tblUsers` `u`
+         JOIN `tblUserSample` `us`
+         JOIN `tblSampleCode` `sc`
+         JOIN `tblLanguages` `l`
+     ON
+     `u`.`pkUsername` = `us`.`fkUsername`
+         AND `us`.`fkSampleId` = sc.`pkSampleId`
+         AND `sc`.`fkSampleLanguage` = `l`.`pkLanguageId`
+     WHERE
+     `u`.`fldConfirmedRegistration` <> 0
+         AND `sc`.`fldPrivate` <> 1";
+
+$stmt = $db->prepare($getSamplesSql);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+<table>
+    <thead>
+    <tr>
+        <th width="150">
+            Author
+        </th>
+        <th width="150">
+            Title
+        </th>
+        <th width="150">
+            Language
+        </th>
+        <th width="150">
+            Description
+        </th>
+        <th width="480">
+            Code
+        </th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+
+    foreach ($rows as $row)
+    {
+        $user = strip_tags($row['user']);
+        $title = strip_tags($row['title']);
+        $lang = strip_tags($row['lang']);
+        $description = strip_tags($row['description']);
+        $code = strip_tags($row['code']);
+        $output =
+            <<<HTML
+                <tr>
+                <td>
+                    {$user}
+                </td>
+                <td>
+                    {$title}
+                </td>
+                <td>
+                    {$lang}
+                </td>
+                <td>
+                    {$description}
+                </td>
+                <td>
+                    <pre>{$code}</pre>
+                </td>
+            </tr>
+HTML;
+        echo $output;
+    }
+    ?>
+    </tbody>
+</table>
+
+
+<?php include "common/bottom.php"; ?>
